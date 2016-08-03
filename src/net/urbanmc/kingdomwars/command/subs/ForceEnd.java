734f2be6d461b35cc3965309c71a93e5e73c4a63
elian1203 <elian1203@gmail.com>
2016-08-03 19:37:02 -0400

    Finished ForceEnd sub

--- src/net/urbanmc/kingdomwars/command/subs/ForceEnd.java
+++ src/net/urbanmc/kingdomwars/command/subs/ForceEnd.java
@@ -3,6 +3,11 @@ package net.urbanmc.kingdomwars.command.subs;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import com.palmergames.bukkit.towny.object.Nation;
+
+import net.urbanmc.kingdomwars.TownyUtil;
+import net.urbanmc.kingdomwars.WarUtil;
+
 public class ForceEnd {
 
 	public ForceEnd(Player p, String[] args) {
@@ -11,6 +16,24 @@ public class ForceEnd {
 			return;
 		}
 
-		
+		if (args.length == 1) {
+			p.sendMessage(ChatColor.RED + "Please specify a nation whose war to end.");
+			return;
+		}
+
+		Nation nation = TownyUtil.getNation(args[1]);
+
+		if (nation == null) {
+			p.sendMessage(ChatColor.RED + "You have not specified a valid nation.");
+			return;
+		}
+
+		if (!WarUtil.inWar(nation)) {
+			p.sendMessage(ChatColor.RED + "That nation is not in a war!");
+			return;
+		}
+
+		WarUtil.end(WarUtil.getWar(nation));
+		p.sendMessage(ChatColor.GOLD + "Ended war.");
 	}
 }
