d769e1bbed464e13112ba823a434fd8437bd6959
S <null>
2016-08-03 08:38:23 -0700

    Added Questioner

--- src/net/urbanmc/kingdomwars/TownyUtil.java
+++ src/net/urbanmc/kingdomwars/TownyUtil.java
@@ -1,9 +1,16 @@
 package net.urbanmc.kingdomwars;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
+
+import ca.xshade.questionmanager.Option;
+import ca.xshade.questionmanager.Question;
+import ca.xshade.questionmanager.QuestionTask;
 
 public class TownyUtil {
 
@@ -30,5 +37,25 @@ public class TownyUtil {
 		}
 
 		return nation;
+	}
+	
+	public void truceQuestion(String playername) {
+	    List<Option> options = new ArrayList<Option>();
+	    options.add(new Option("accept", new QuestionTask() {
+	      public void run() {
+	        
+	      }
+	    }));
+	    options.add(new Option("deny", new QuestionTask() {
+	      public void run() {
+	        
+	      }
+	    }));
+	    Question question = new Question(playername, "Would you like to accept a truce with %nation%? You will receive %configTruceAmount% from their nation bank.", options);
+	    try {
+	      KingdomWars.questionerplugin.getQuestionManager().appendQuestion(question);
+	    } catch (Exception e) {
+	      System.out.println(e.getMessage());
+	    }
 	}
 }
