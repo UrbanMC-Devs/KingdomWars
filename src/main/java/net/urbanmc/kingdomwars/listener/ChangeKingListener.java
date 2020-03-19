471bee81e3cbbee9ba7b10099c8030dbbaac5f17
S <null>
2020-03-18 23:48:03 -0700

    Fix being able to change king by changing capital mayor.

--- src/main/java/net/urbanmc/kingdomwars/listener/ChangeKingListener.java
+++ src/main/java/net/urbanmc/kingdomwars/listener/ChangeKingListener.java
@@ -1,6 +1,9 @@
 package net.urbanmc.kingdomwars.listener;
 
 import com.palmergames.bukkit.towny.event.NationChangeLeaderEvent;
+import com.palmergames.bukkit.towny.event.TownChangeMayorEvent;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Nation;
 import net.urbanmc.kingdomwars.KingdomWars;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -20,6 +23,33 @@ public class ChangeKingListener implements Listener {
     public void onNationChangeKing(NationChangeLeaderEvent event) {
         // Check if nation is in war
         if (plugin.getWarManager().inWar(event.getNation())) {
+            // Cancel event (prevent leader change) if nation is in war
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onTownChangeMayor(TownChangeMayorEvent event) {
+        // Check if town has nation
+        if (!event.getTown().hasNation())
+            return;
+
+        // Get nation
+        Nation nat = null;
+
+        try {
+            nat = event.getTown().getNation();
+        } catch (NotRegisteredException ignore) {
+            return;
+        }
+
+        // Check
+        if (!nat.getCapital().equals(event.getTown())) {
+            return;
+        }
+
+        // Check if nation is in war
+        if (plugin.getWarManager().inWar(nat)) {
             // Cancel event (prevent leader change) if nation is in war
             event.setCancelled(true);
         }
